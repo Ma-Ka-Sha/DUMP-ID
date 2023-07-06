87b9bd78609646e7abee8b159c5bdbfb73829c7a
Ma-Ka-Sha <136921706+Ma-Ka-Sha@users.noreply.github.com>
2023-07-06 16:45:11 +0630

    Add files via upload

---
+++ run_64.py
@@ -0,0 +1,8 @@
+import os, platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if "64bit" in bit:
+    from File_enc import main
+    main()
+else:
+    print("[×] Sorry your device is not support")
