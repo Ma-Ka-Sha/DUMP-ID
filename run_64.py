40882595d72ce3fb223ccba7bbdc7358e1c4adcd
Ma-Ka-Sha <136921706+Ma-Ka-Sha@users.noreply.github.com>
2023-07-06 17:03:56 +0630

    run_64.py

--- run_64.py
+++ run_64.py
@@ -2,7 +2,7 @@ import os, platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if "64bit" in bit:
-    from File_enc import main
+    from File_64_enc import main
     main()
 else:
     print("[×] Sorry your device is not support")
